06166e81202f40e9ffb38c1d98d94fedde9650ba
prmothe <prmothe@deloitte.com>
2023-08-01 21:00:10 +0530

    boook-my-seat
    1. CSS added
    2. Router improved
    3, AppLayout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { AppHeader } from './components/AppHeader';
-import { Route, Routes } from 'react-router-dom';
-import  Booking  from "./components/Booking"
-import { OfficeAdmin } from "./components/OfficeAdmin"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { AppHeader } from "./components/AppHeader";
+import { Route, Routes } from "react-router-dom";
+import Booking from "./components/Booking";
+import { OfficeAdmin } from "./components/OfficeAdmin";
+import { AppLayout } from "./components/AppLayout";
 
 function App() {
   return (
     <>
-      
-      <div className="App">
+      <div className="App bg-[#F0F2F3]">
         <AppHeader />
       </div>
       <Routes>
-        <Route path='booking' element={<Booking />} ></Route>
-        <Route path='admin' element={<OfficeAdmin />} ></Route>
+        <Route path="/" element={<AppLayout />}>
+          <Route index path="/" element={<Booking />}></Route>
+          <Route path="booking" element={<Booking />}></Route>
+          <Route path="admin" element={<OfficeAdmin />}></Route>
+        </Route>
       </Routes>
     </>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className = 'bg-black  text-center m-auto p-4 h-14 text-white'>@Book-My-seat</div>
+  )
+}
