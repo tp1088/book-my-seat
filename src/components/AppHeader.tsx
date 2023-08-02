f24405fa6a583dbf1ba762c4adc37520f5787049
prmothe <prmothe@deloitte.com>
2023-08-02 12:26:21 +0530

    Revert "boook-my-seat"
    This reverts commit 06166e81202f40e9ffb38c1d98d94fedde9650ba.

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,25 +1,17 @@
-import {  Link, Outlet } from "react-router-dom";
-import "../App.css";
+import { useNavigate, Link, Outlet } from "react-router-dom";
+import '../App.css';
 import { useEffect } from "react";
 import { fetchInitial } from "../services/seatServices";
 
 export const AppHeader = () => {
-  useEffect(() => {
-    fetchInitial();
-  }, []);
-  return (
-    <header className="flex justify-between bg-black text-white p-4 h-14">
-      <div>
-        <p>Book-My-Seat</p>
-      </div>
-      <nav>
-        <Link className="tab" to={"booking"}>
-          Booking
-        </Link>
-        <Link className="tab" to={"admin"}>
-          Office Admin
-        </Link>
-      </nav>
-    </header>
-  );
-};
+    useEffect(() => {
+        fetchInitial()
+    }, [])
+    return (
+        <div className="header">
+            <Link className="tab" to={'Booking'}  >Booking</Link>
+            <Link className="tab" to={'admin'} >Office Admin</Link>
+            <Outlet />
+        </div>
+    )
+}
--- src/components/OfficeAdmin.tsx
+++ src/components/OfficeAdmin.tsx
@@ -1,25 +1,29 @@
-import { useEffect } from "react";
+
+import { useEffect} from "react";
 import React from "react";
-import { DataGrid, GridPaginationModel, GridColDef } from "@mui/x-data-grid";
+import { DataGrid, GridPaginationModel, GridColDef } from '@mui/x-data-grid';
 import { fetchCurrentBookings } from "../services/seatServices";
 
-export const OfficeAdmin = () => {
-  const [rows, setRows] = React.useState(() => []);
-  const columns: GridColDef[] = [
-    { field: "user", headerName: "User", width: 250 },
-    { field: "floor", headerName: "Floor", width: 150 },
-    { field: "seat", headerName: "Seat", width: 150 },
-    { field: "bookingDate", headerName: "Booking Date", width: 250 },
-  ];
-  useEffect(() => {
-    setRows(fetchCurrentBookings);
-  }, []);
-  return (
-    <div className="">
-      <h2 className = 'text-left text-3xl'> Booking Details</h2>
-      <section className = 'w-2/3 m-auto'>
-        <DataGrid rows={rows} columns={columns} />
-      </section>
-    </div>
-  );
-};
+
+export const OfficeAdmin = ()=>{
+    const [rows, setRows] = React.useState(() => []);
+    const columns: GridColDef[] = [
+        { field: 'user', headerName: 'User', width: 250 },
+        { field: 'floor', headerName: 'Floor', width: 150 },
+        { field: 'seat', headerName: 'Seat', width: 150 },
+        { field: 'bookingDate', headerName: 'Booking Date', width: 250 }
+    ];
+    useEffect(() => {
+        setRows(fetchCurrentBookings)
+    }, [])
+    return (
+        <div>
+            <h1> Booking Details</h1>
+
+            <DataGrid
+                rows={rows}
+                columns={columns}
+            />
+        </div>
+    )
+}
