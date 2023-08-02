c8f00f4976e920d8864b0ad5b1dc5ef687c2cf23
Pandit <tpandit@deloitte.com>
2023-08-02 19:05:30 +0530

    pagination

--- src/components/OfficeAdmin.tsx
+++ src/components/OfficeAdmin.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import { DataGrid, GridPaginationModel, GridColDef } from "@mui/x-data-grid";
 import { fetchCurrentBookings } from "../services/seatServices";
 import './OfficeAdmin.css';
+import { useState } from "react";
 
 export const OfficeAdmin = () => {
   const [rows, setRows] = React.useState(() => []);
   const columns: GridColDef[] = [
-    { field: "email", headerName: "User", width: 250 },
+    { field: "email", headerName: "User", width: 250 , },
     { field: "floor", headerName: "Floor", width: 150 },
     { field: "seat", headerName: "Seat", width: 150 },
     { field: "bookingDate", headerName: "Booking Date", width: 250 },
@@ -15,11 +16,20 @@ export const OfficeAdmin = () => {
   useEffect(() => {
     setRows(fetchCurrentBookings);
   }, []);
+  const [paginationModel, setPaginationModel] = useState<GridPaginationModel>({
+    page: 0,
+    pageSize: 5
+  })
   return (
     <div>
       <h1 className="text-3xl mb-5  font-semibold"> Booking Details</h1>
       <div className="w-2/3 m-auto bg-slate-50 opacity-90">
-        <DataGrid rows={rows} columns={columns} />
+        <DataGrid rows={rows} columns={columns} 
+        pagination
+        paginationMode='client'
+        paginationModel={paginationModel}
+        onPaginationModelChange={(paginationModel: GridPaginationModel) => setPaginationModel(paginationModel)}
+        pageSizeOptions={[5, 10, 25]}/>
       </div>
     </div>
   );
